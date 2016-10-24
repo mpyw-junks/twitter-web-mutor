73ab15fbdc0d9ae6da95e3ed63d14820f0005bbc
mpyw <ryosuke_i_628@yahoo.co.jp>
2016-10-24 13:29:28 +0900

    ES2015ベースに変更

--- contents/content.js
+++ contents/content.js
@@ -1,188 +1,184 @@
 'use strict';
 
-(function (iframe) {
+(iframe => {
 
-    iframe.style.visibility = 'hidden';
-    document.body.appendChild(iframe);
-    iframe.onload = function () {
+  iframe.style.visibility = 'hidden';
+  document.body.appendChild(iframe);
 
-        /* Sandboxed contentWindow */
-        var sandbox = this.contentWindow;
+  iframe.addEventListener('load', () => {
 
-        /* Retryable document.getElementById() */
-        var RetryableGetElementById = function self(id, maxRetries, delay) {
-            return new Promise(function (resolve) {
-                var element = document.getElementById(id);
-                if (element) {
-                    resolve(element);
-                } else if (maxRetries-- > 0) {
-                    setTimeout(function () {
-                        self(id, maxRetries, delay).then(function (element) {
-                            return resolve(element);
-                        });
-                    }, delay);
-                }
-            });
-        };
+    /* Sandboxed contentWindow */
+    const sandbox = iframe.contentWindow;
 
-        /* Callback managing functions */
-        var RestoreCallbackCode = function () {
-            return localStorage['tcm-callback'] || 'return false;';
-        };
-        var StoreCallbackCode = function (code) {
-            localStorage['tcm-callback'] = code;
+    /* Retryable document.getElementById() */
+    const retryableGetElementById = (id, maxRetries, delay) => {
+      return new Promise(resolve => {
+        const e = document.getElementById(id);
+        if (e) {
+          resolve(e);
+        } else if (maxRetries-- > 0) {
+          setTimeout(() => {
+            retryableGetElementById(id, maxRetries, delay).then(e => resolve(e));
+          })
         }
-        var SetCallback = function (code) {
-            sandbox.postMessage({
-                actionType: 'set-callback',
-                code: code
-            }, '*');
-        };
-        var GoCallback = function (item) {
-            if (
-                item.dataset.itemType !== 'tweet' &&
-                item.dataset.componentContext !== 'reply_activity'
-            ) {
-                return true;
-            }
-            var tweet = item.querySelector('.tweet');
-            var retweeter = tweet.dataset.retweeter;
-            var retweetInfo = item.querySelector('.js-retweet-text');
-            var params = [
-                tweet.dataset.userId,
-                tweet.dataset.screenName,
-                tweet.dataset.name,
-                tweet.querySelector('.tweet-text').textContent,
-                !!tweet.dataset.promoted,
-                !!retweeter,
-                retweeter ? retweetInfo.querySelector('a').dataset.userId : null,
-                retweeter,
-                retweeter ? retweetInfo.querySelector('b').textContent : null,
-                item.parentNode.parentNode.className.slice(7, -7)
-            ];
-            sandbox.postMessage({
-                actionType: 'go-callback',
-                itemId: item.id,
-                params: params
-            }, '*');
-        }
-
-        /* Observers */
-        var CreateObserver = function (nodeType, options, filterCallback, applyCallback) {
-            var observer = new MutationObserver(function (mutations) {
-                var filterCallbackLocal = filterCallback;
-                var applyCallbackLocal = applyCallback;
-                var filterCallbackInvalid = typeof filterCallbackLocal !== 'function';
-                if (typeof applyCallbackLocal !== 'function') {
-                    return;
-                }
-                for (var i = mutations.length; i--;) {
-                    var mutation = mutations[i];
-                    for (var j = mutation[nodeType].length; j--;) {
-                        if (filterCallbackInvalid || filterCallbackLocal(mutation[nodeType][j])) {
-                            applyCallbackLocal(mutation[nodeType][j]);
-                        }
-                    }
-                }
-            });
-            var _observe = observer.observe;
-            observer.observe = function (target) {
-                return _observe.call(this, target, options);
-            };
-            return observer;
-        };
-        var PreObserver = {
-            observe: function (node) {
-                var childs = node.children;
-                for (var i = childs.length; i--;) {
-                    GoCallback(childs[i]);
-                }
-            }
-        };
-        var MuteObserver = CreateObserver(
-            'addedNodes',
-            {childList: true},
-            function (e) { return e.tagName === 'LI'; },
-            function (e) { GoCallback(e); }
-        );
-        var RefreshObserver = CreateObserver(
-            'addedNodes',
-            {childList: true, subtree: true},
-            function (e) { return e.id === 'timeline'; },
-            function (e) {
-                MuteObserver.disconnect();
-                RetryableGetElementById('stream-items-id', 5, 500).then(function (streamItems) {
-                    PreObserver.observe(streamItems);
-                    MuteObserver.observe(streamItems);
-                });
-            }
-        );
-
-        /* Initialize callback */
-        SetCallback(RestoreCallbackCode());
-
-        /* Start listeing */
-        window.addEventListener('message', function (e) {
-            if (e.data.actionType === 'tcm-remove-tweet') {
-                var node = document.getElementById(e.data.itemId);
-                node.parentNode.removeChild(node);
-            }
-        });
-
-        /* Start observing */
-        RetryableGetElementById('stream-items-id', 5, 500).then(function (streamItems) {
-            PreObserver.observe(streamItems);
-            MuteObserver.observe(streamItems);
-        });
-        RefreshObserver.observe(document);
-
-        /* Insert mute button */
-        document.styleSheets[0].insertRule('.Icon--volume-off:before { content: "\\f056"}', 0);
-        RetryableGetElementById('global-actions', 0, 0).then(function (topbar) {
-            var icon = document.createElement('span');
-                icon.className = 'Icon Icon--volume-off Icon--large';
-            var text = document.createElement('span');
-                text.className = 'text';
-                text.textContent = 'ミュート';
-            var a = document.createElement('a');
-                a.addEventListener('click', function (e) {
-                    e.preventDefault();
-                    var code = window.prompt(
-                        "ミュート判定関数をJavaScriptで記述.\n" +
-                        "以下の変数が参照可能.\n" +
-                        "\n" +
-                        "user_id_str(string)\n" +
-                        "screen_name(string)\n" +
-                        "name(string)\n" +
-                        "text(string)\n" +
-                        "is_promoted(boolean)\n" +
-                        "is_retweeted(boolean)\n" +
-                        "retweeter_user_id_str(string | null)\n" +
-                        "retweeter_screen_name(string | null)\n" +
-                        "retweeter_name(string | null)\n" +
-                        "stream_type(string)\n" +
-                        "↑ \"home\", \"connect\", \"discover\", \"search\"\n" +
-                        "\n" +
-                        "ミュートする場合: return true;\n" +
-                        "残したい場合: return false;\n",
-                        RestoreCallbackCode()
-                    );
-                    if (code !== null) {
-                        StoreCallbackCode(code);
-                        SetCallback(code);
-                    }
-                });
-                a.className = 'js-nav js-tooltip js-dynamic-tooltip';
-                a.href = '';
-                a.appendChild(icon);
-                a.appendChild(text);
-            var li = document.createElement('li');
-                li.id = 'mute-nav-id';
-                li.appendChild(a);
-            topbar.appendChild(li);
-        });
-
+      });
     };
-    iframe.setAttribute('src', chrome.extension.getURL('sandboxes/sandbox.html'));
+
+    /* Callback managing functions */
+    const restoreCallbackCode = () => {
+      return localStorage['twm-callback'] || 'return false;';
+    };
+    const storeCallbackCode = code => {
+      localStorage['twm-callback'] = code;
+    };
+    const setCallback = code => {
+      sandbox.postMessage({
+        actionType: 'twm-set-callback',
+        code,
+      }, '*');
+    };
+    const execCallback = item => {
+      if (item.dataset.itemType !== 'tweet' &&
+          item.dataset.componentContext !== 'reply_activity') {
+        return true;
+      }
+      const tweet = item.querySelector('.tweet');
+      const retweeter = tweet.dataset.retweeter;
+      const retweetInfo = item.querySelector('.js-retweet-text');
+      const params = [
+        tweet.dataset.userId,
+        tweet.dataset.screenName,
+        tweet.dataset.name,
+        tweet.querySelector('.tweet-text').textContent,
+        !!tweet.dataset.promoted,
+        !!retweeter,
+        retweeter ? retweetInfo.querySelector('a').dataset.userId : null,
+        retweeter,
+        retweeter ? retweetInfo.querySelector('b').textContent : null,
+        item.parentNode.parentNode.className.slice(7, -7)
+      ];
+      sandbox.postMessage({
+        actionType: 'twm-exec-callback',
+        itemId: item.id,
+        params,
+      }, '*');
+    }
+
+    /* Observers */
+    const createPseudoObserver = (nodeType, options, filterCallback, applyCallback) => {
+      const observer = new MutationObserver(mutations => {
+        const filterCallbackLocal = filterCallback;
+        const applyCallbackLocal = applyCallback;
+        const filterCallbackInvalid = typeof filterCallbackLocal !== 'function';
+        if (typeof applyCallbackLocal !== 'function') {
+          return;
+        }
+        for (const mutation of mutations) {
+          for (const item of mutation[nodeType]) {
+            if (filterCallbackInvalid || filterCallbackLocal(item)) {
+              applyCallbackLocal(item);
+            }
+          }
+        }
+      });
+      return {
+        observe: target => observer.observe(target, options),
+        disconnect: () => observer.disconnect(),
+      };
+    };
+    const preObserver = {
+      observe: target => {
+        for (const child of target.children) {
+          execCallback(child);
+        }
+      },
+    };
+    const listObserver = createPseudoObserver(
+      'addedNodes',
+      {childList: true},
+      e => e.tagName === 'LI',
+      e => execCallback(e)
+    );
+    const refreshObserver = createPseudoObserver(
+      'addedNodes',
+      {childList: true, subtree: true},
+      e => e.id === 'timeline',
+      e => {
+        listObserver.disconnect();
+        retryableGetElementById('stream-items-id', 5, 500).then(streamItems => {
+          preObserver.observe(streamItems);
+          listObserver.observe(streamItems);
+        });
+      }
+    );
+
+    /* Initialize callback */
+    setCallback(restoreCallbackCode());
+
+    /* Start listeing */
+    addEventListener('message', e => {
+      switch (e.data.actionType) {
+        case 'twm-remove-tweet':
+          const node = document.getElementById(e.data.itemId);
+          node.parentNode.removeChild(node);
+          break;
+      }
+    });
+
+    /* Start observing */
+    retryableGetElementById('stream-items-id', 5, 500).then(streamItems => {
+      preObserver.observe(streamItems);
+      listObserver.observe(streamItems);
+    });
+    refreshObserver.observe(document);
+
+    /* Insert mute button */
+    document.styleSheets[0].insertRule('.Icon--volume-off:before { content: "\\f056"}', 0);
+    retryableGetElementById('global-actions', 0, 0).then(topbar => {
+      const icon = document.createElement('span');
+            icon.className = 'Icon Icon--volume-off Icon--large';
+      const text = document.createElement('span');
+            text.className = 'text';
+            text.textContent = 'ミュート';
+      const a = document.createElement('a');
+            a.addEventListener('click', e => {
+              e.preventDefault();
+              const code = window.prompt(
+                "ミュート判定関数をJavaScriptで記述。\n" +
+                "以下の変数が参照可能。\n" +
+                "\n" +
+                "user_id_str(string)\n" +
+                "screen_name(string)\n" +
+                "name(string)\n" +
+                "text(string)\n" +
+                "is_promoted(boolean)\n" +
+                "is_retweeted(boolean)\n" +
+                "retweeter_user_id_str(string | null)\n" +
+                "retweeter_screen_name(string | null)\n" +
+                "retweeter_name(string | null)\n" +
+                "stream_type(string)\n" +
+                "↑ \"home\", \"connect\", \"discover\", \"search\"\n" +
+                "\n" +
+                "ミュートする場合: return true;\n" +
+                "残したい場合: return false;\n",
+                restoreCallbackCode()
+              );
+              if (code !== null) {
+                storeCallbackCode(code);
+                setCallback(code);
+              }
+            });
+            a.className = 'js-nav js-tooltip js-dynamic-tooltip';
+            a.href = '';
+            a.appendChild(icon);
+            a.appendChild(text);
+        const li = document.createElement('li');
+              li.id = 'mute-nav-id';
+              li.appendChild(a);
+        topbar.appendChild(li);
+    });
+
+  });
+  iframe.setAttribute('src', chrome.extension.getURL('sandboxes/sandbox.html'));
 
 })(document.createElement('iframe'));
--- sandboxes/sandbox.js
+++ sandboxes/sandbox.js
@@ -1,33 +1,38 @@
-(function () {
+(() => {
 
-    var callback = function () { return false; };
+  let callback = () => false;
 
-    window.addEventListener('message', function (e) {
-        switch (e.data.actionType) {
-            case 'set-callback':
-                callback = new Function(
-                    'user_id_str',
-                    'screen_name',
-                    'name',
-                    'text',
-                    'is_promoted',
-                    'is_retweeted',
-                    'retweeter_user_id_str',
-                    'retweeter_screen_name',
-                    'retweeter_name',
-                    'stream_type',
-                    e.data.code
-                );
-                break;
-            case 'go-callback':
-                if (callback.apply({}, e.data.params)) {
-                    e.source.postMessage({
-                        actionType: 'tcm-remove-tweet',
-                        itemId: e.data.itemId,
-                    }, e.origin);
-                }
-                break;
+  addEventListener('message', e => {
+
+    switch (e.data.actionType) {
+
+      case 'twm-set-callback':
+        callback = new Function(
+          'user_id_str',
+          'screen_name',
+          'name',
+          'text',
+          'is_promoted',
+          'is_retweeted',
+          'retweeter_user_id_str',
+          'retweeter_screen_name',
+          'retweeter_name',
+          'stream_type',
+          e.data.code
+        );
+        break;
+
+      case 'twm-exec-callback':
+        if (callback(...e.data.params)) {
+          e.source.postMessage({
+            actionType: 'twm-remove-tweet',
+            itemId: e.data.itemId,
+          }, e.origin);
         }
-    });
+        break;
+
+    }
+
+  });
 
 })();
